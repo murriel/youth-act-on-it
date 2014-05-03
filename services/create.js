21a702738931ec5ea78227aa68aa87c59002ac0f
acohen <arik.cohen@mgo.com>
2014-05-03 16:17:42 -0700

    added create service

--- services/create.js
+++ services/create.js
@@ -1,10 +1,29 @@
 var db=require('../lib/db');
 
 module.exports=function(req,res) {
-  db.query('insert into activity SET ?',req.body,function(err,results) {
+  var activity=req.body;
+  db.query('insert into activity (title,' +
+           '                      description,' +
+           '                      create_date,' +
+           '                      activity_location,' +
+           '                      longitude,' +
+           '                      latitude,' +
+           '                      status) ' +
+           '            values ($1,$2,$3,$4,$5,$6,$7)',
+           [activity.title,
+            activity.description,
+            activity.create_date,
+            activity.activity_location,
+            activity.longitude,
+            activity.latitude,
+            activity.status],
+
+  function(err,results) {
+
     if(err) {
       throw err;
     }
+
     res.send(200);
   });
 };
