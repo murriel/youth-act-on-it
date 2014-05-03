ac02ef3266be11bd29bb4bee32441518d0f48d80
acohen <arik.cohen@mgo.com>
2014-05-03 15:24:56 -0700

    finished the /activity/list service

--- services/list.js
+++ services/list.js
@@ -1,5 +1,16 @@
 var db=require('../lib/db');
 
 module.exports=function(req,res) {
-  res.send(200);
+
+  db.query('select * from activity',[],function(err,results) {
+    if(err) {
+       console.log(err.message);
+       return res.send(500);
+    }
+    var object={};
+    object.activities=results.rows;
+    object.total=results.length;
+    res.send(200,object);
+  });
+
 };
