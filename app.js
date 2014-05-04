761d5ceae7ebd7fc9167cd6fcea75eae896e1758
acohen <arik.cohen@mgo.com>
2014-05-04 13:41:26 -0700

    fixed limit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || config.port);
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
