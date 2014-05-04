f43a5bf2e2376184f77ed417ccc66aabc4ca6f67
ggoussard <guillaume.goussard@mgo.com>
2014-05-04 13:29:27 -0700

    Add picture support from camera

--- public/js/create_activity.js
+++ public/js/create_activity.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 				longitude: 10,
 				latitude: 10,
 				status:"APPROVED",
-				visual_asset: ""
+				visual_asset: $("#show-picture").prop("src")
 			},
 			type: "POST",
 			complete: function(){
@@ -18,7 +18,7 @@ $(document).ready(function(){
 		});
 	});
 
-	$("#getLocation").click(function(){
+    $("#getLocation").click(function(){
         navigator.geolocation.getCurrentPosition(function(pos){
             $("#activity_location").val(pos.coords.latitude + ',' + pos.coords.longitude);
         },
@@ -26,6 +26,37 @@ $(document).ready(function(){
             $("#activity_location").val("Location cannot be found");
         });
     });
+
+    var takePicture = document.querySelector("#take-picture"),
+        showPicture = document.querySelector("#show-picture");
+
+    if (takePicture && showPicture) {
+        // Set events
+        takePicture.onchange = function (event) {
+            // Get a reference to the taken picture or chosen file
+            var files = event.target.files,
+                file;
+            if (files && files.length > 0) {
+                file = files[0];
+                try {
+                    // Fallback if createObjectURL is not supported
+                    var fileReader = new FileReader();
+                    fileReader.onload = function (event) {
+                        showPicture.src = event.target.result;
+                        showPicture.hidden = false;
+                    };
+                    fileReader.readAsDataURL(file);
+                }
+                catch (e) {
+                    //
+                    var error = document.querySelector("#error");
+                    if (error) {
+                        error.innerHTML = "Neither createObjectURL or FileReader are supported";
+                    }
+                }
+            }
+        };
+    }
 });
 
 
